2a84da23157c662b1509cf18c4c4e00e560ebf9a
Rajan Khade <rajankhade31@gmail.com>
2023-02-07 16:36:39 +0530

    update: client SPA

--- client/components/Navbar.vue
+++ client/components/Navbar.vue
@@ -1,3 +1,29 @@
+<script setup>
+const router = useRouter();
+const userName = inject('userName');
+
+const handleLogout = () => {
+    fetch('http://localhost:3001/api/v1/logout',
+        {
+            method: 'POST',
+            credentials: 'include',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+
+        })
+        .then(res => {
+            if (res.status === 200) {
+                userName.value = '';
+                router.push('/login');
+            }
+            else {
+                alert('Logout Failed');
+            }
+        });
+};
+</script>
 <template>
     <nav>
         <ul>
@@ -7,6 +33,14 @@
             <li>
                 <NuxtLink to="/about">About</NuxtLink>
             </li>
+            <ClientOnly fallback-tag="span" fallback="Showing User info">
+                <li v-if="userName">{{ userName }}</li>
+                <li v-if="userName"><button @click="handleLogout">Logout</button></li>
+                <li v-else>
+                    <NuxtLink to="/login">Login</NuxtLink>
+                </li>
+            
+            </ClientOnly>
         </ul>
     </nav>
 </template>
